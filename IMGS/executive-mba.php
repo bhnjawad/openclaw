e9d0f1c131267dd602fa1a09d3a3bae7c8fb0f82
bhnjawad <bjawadwala.52@gmail.com>
2026-04-06 10:04:17 +0530

    images random

---
+++ IMGS/executive-mba.php
@@ -0,0 +1,251 @@
+ 
+<!-- Default Theme 
+<link rel="stylesheet" href="owl-carousel/owl.theme.css">-->
+ 
+<!--  jQuery 1.7+ 
+<script src="jquery-1.9.1.min.js"></script> -->
+ 
+<!-- Include js plugin 
+<script src="assets/owl-carousel/owl.carousel.js"></script>-->
+<style>
+	@keyframes blink {
+		0% {
+			opacity: 1;
+		}
+		50% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.blinking-text {
+		animation: blink 1s infinite;
+	}
+
+	.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+
+<div class="container">
+  <div class="row">
+	<img src="images/innerpageb.jpg" width="100%" class="img-responsive" />
+  </div>
+  <div class="row">
+	<div class="container-fluid">
+	<div class="inner-box"><H2 class="head-inner">Executive MBA</H2></div>
+	
+	</div>
+	<div class="container-fluid">
+		<div class="col-lg-9" style="height:600px; overflow:auto;">
+
+
+
+<p>The EMBA programme enables working executives to upgrade their managerial knowledge and skills while continuing to be employed. This rigorous programme is conducted through regular classes on weekends. The programme consists of a compulsory set of core subjects and a variety of elective subjects. The participants bring in their wide experiences into the classroom and work on regular individual and group projects. Throughout the programme, participants practice the concepts learnt in classrooms at their workplace. This parallel learning and implementation process form the basis of this programme.</p>
+
+<H4>Why EMBA Programme at VGSoM ?</H4>
+
+
+
+<UL>
+	<LI>•	To fast-track the career of senior and mid-career executives</LI>
+	<LI>•	To achieve leadership orientation for taking organisations to the next level</LI>
+	<LI>•	For networking opportunities through IIT’s extensive alumni network</LI>
+
+</UL>
+
+<H4>Benefits and impact on your career</H4>
+
+<p>VGSoM's Executive MBA sets you apart and gives you a genuine edge. Whether your goal is to progress within your organization, take on new or global responsibilities, or pursue an entrepreneurial route, the programme will give you the knowledge, insights and confidence to operate successfully anywhere in the world.</p>
+
+<p>Studying while you remain in full-time employment enables you to improve your workplace performance from day one as you apply each new classroom concept with direct relevance to you and your organization. The practical business expertise and the new ways of management thinking that you develop at the School change your approach to decision making and give you the ideas, inspiration and contacts to shape your future career.</p>
+
+
+<UL>
+	<LI>•	You are able to move up in your career quickly and with confidence.</LI>
+	<LI>•	You can successfully make the transition to general management from a technical or functional role.</LI>
+	<LI>•	The elective portfolio allows you to specialize in certain subject areas or gain breadth across a range of business and management disciplines.</LI>
+	<LI>•	You gain entrepreneurial skills to help you develop your own venture, or to implement new and innovative ideas within your organization.</LI>
+	<LI>•	You are able to build a strong network of personal and professional contacts.</LI>
+	<LI>•	You continue to benefit from the School and IIT's reputation, recognition and global networks well beyond your time in the programme</LI>
+
+</UL>
+
+
+
+
+<!-- <table class="table table-bordered" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px;">
+	<thead style="box-sizing: border-box;">
+		<tr style="box-sizing: border-box;">
+			<th>Key Points</th>
+			<th>Description</th>
+			<th>Details</th>
+		</tr>
+	</thead>
+	<tbody style="box-sizing: border-box;">
+		<tr style="box-sizing: border-box;">
+			<td>
+				Primary Study Location(s)</td>
+			<td>
+				<UL>
+					<LI>•	Kolkata</LI>
+					<LI>•	Jamshedpur</LI>
+				</UL>
+		</td>
+			<td>
+				1-weekend per term is held at IIT Campus at Kharagpur</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Duration</td>
+			<td>
+			<UL>
+					<LI>•	3 years (till Batch 2023-26)</LI>
+					<LI>•	2 years (from Batch 2024-26)</LI>
+				</UL></td>
+			<td>
+				&nbsp;</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				September</td>
+			<td>
+				&nbsp;</td>
+			<td>
+				2-years Course Work and Project Work</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Format</td>
+			<td>
+				Study whilst working full time</td>
+			<td>
+				Study during weekends</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Course Delivery</td>
+			<td>
+				Weekend Classes</td>
+			<td>
+				15 hours per weekend classes with regular quizzes , Case study analysis and presentations along with group tasks</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Start Date</td>
+			<td>
+				14-15 September, 2024</td>
+			<td>
+				&nbsp;</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Degree Awarded</td>
+			<td>
+				MBA</td>
+			<td>
+				&nbsp;</td>
+		</tr>
+		<tr style="box-sizing: border-box;">
+			<td>
+				Career/Placement</td>
+			<td>
+				</td>
+			<td>
+				Placement process is student driven</td>
+		  </tr>
+		  <tr style="box-sizing: border-box;">
+			<td>
+				Tuition Fees</td>
+			<td>
+				18,00,000 INR</td>
+			<td>
+				&nbsp;</td>
+		  </tr>
+		 </tbody>
+		</table> -->
+
+		<H2 id="admission" class="head-inner">Admissions - Executive MBA</H2>
+		<H3 id="admission" class="head-inner">2 Years Course</H3>
+
+		<!-- <p><A HREF="pdf/emba_instructions.pdf" target="blank">EMBA Admission 2024-2026: Important Instructions</A></p> -->
+
+		<!-- <p><A HREF="emba_instructions.php" target="blank">EMBA Admission 2024-2026: Important Instructions</A></p> -->
+
+		<!-- <p class="blinking-text"><b><A style="color:red" HREF="https://erp.iitkgp.ac.in/IITKGPApplications/index?admiss_ch=15" target="blank">Online Application for Executive MBA Programme 2024-2026</A></b></p> -->
+
+		<p><A HREF="pdf/sample_question_paper.pdf" target="blank">EMBA Admission Sample Question Paper</A></p>
+		
+		<!-- <p><A HREF="EMBA-Placement-Brochure-2020" target="blank">Executive MBA Placement Brochure</A></p> -->
+		<p><A HREF="pdf/emba_curriculum.pdf" target="blank">EMBA Course Curriculum from 2024-2026 onwards</A></p>
+
+		<!-- <p><A HREF="https://erp.iitkgp.ac.in/ERPWebServices/curricula/CurriculaSubjectsList.jsp?stuType=PG&splCode=BM1" target="blank">EMBA Course Curriculum upto 2023-2026 batch</A></p> -->
+
+		
+		<!-- <p><A HREF="pdf/VGSoM_MBA PROGRAMME PartTime 2-Year Programme Brochure - updated.pdf" target="blank">EMBA Admission 2024-2026 Brochure</A></p> -->
+		<p><B><A HREF="pdf/VGSoM MBA-Ex 2025_Brochure.pdf" target="blank">EMBA Admission 2025-2027 Brochure</A></B></p>
+
+
+		<p class="blinking-text"><b><A style="color:Blue" HREF="" target="blank">Online Application Portal: Apply by 10th July 2025</A></b></p>
+
+<button class="button"><b><A style="color:red" HREF="https://erp.iitkgp.ac.in/IITKGPApplications/index?admiss_ch=15" target="blank">Click here</button></A></b></p>
+		<!-- <p><A HREF="pdf/emba_faqs_2024.pdf" target="blank">EMBA FAQs</A></p> -->
+		<!-- <p><A HREF="emba_faq.php" target="blank">EMBA FAQs</A></p> -->
+
+
+		<H4>EMBA Admissions Committee</H4>
+
+		<!-- <p>The Admissions Committee in VGSoM is responsible for working closely with the institute authorities to bring out the application form, creating channels for communication with candidates at all stages of the admission process, helping aspirants clear all their queries and concerns, and helping them make an informed decision.</p> -->
+
+		<p>The Executive MBA program at VGSoM is tailored for working professionals looking to advance their managerial skills while maintaining full-time employment. With classes held on weekends, participants can balance work and study, engaging in both core and elective subjects. The program fosters a dynamic learning environment where diverse experiences enrich classroom discussions and projects. This setup enables participants to apply new concepts directly to their workplace, ensuring a continuous learning and implementation cycle.</p>
+		<p>Choosing VGSoM’s EMBA provides a fast track to career advancement, equipping you with leadership skills to elevate your organization. It also offers valuable networking opportunities through IIT KGP’s extensive alumni network, enhancing your professional connections. The program distinguishes you in the global business landscape, providing the insights and confidence needed for success, whether you aim to climb the corporate ladder, take on international responsibilities, or pursue entrepreneurship.</p>
+		<p>By studying while working, you can immediately apply classroom concepts to improve workplace performance. The program helps you transition from technical roles to general management, and the diverse elective offerings allow for specialization or a broad understanding of various business disciplines. You'll gain entrepreneurial skills to drive innovation within your organization or start your own venture. The strong network and prestigious reputation of VGSoM and IIT KGP offer long-term benefits, supporting your career well beyond your time in the program.</p>
+
+		<BR><br>
+		
+
+		Contact details:<BR>&nbsp;<BR>
+		EMBA Admission Office: +91 3222 282297, 95<BR>
+		Email: executivemba@vgsom.iitkgp.ac.in
+
+	</div>
+		
+		<!-- <div class="col-lg-3">
+		<H4>MBA</H4>
+			<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"><span></i><a href="/MBA"  target="blank">Admission</a><span></p>
+			<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"><span></i><a href="pdf/mba_curriculum.pdf" target="blank">Curriculum</a><span></p>
+			<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"><span></i><a href="MBA/files/MBA%20Fee%20structure-2022-2024.pdf" target="blank">Fees and Financing</a><span></p> -->
+
+		<!-- <H4>Executive MBA</H4>
+
+		<p><i class="fa fa-chevron-right fa-1"></i><span><a href="executive-mba.php">Overview</a></span></p>
+		<p><i class="fa fa-chevron-right fa-1"></i><span><a href="executive-mba.php#admission">Admission</a></span></p> -->
+		<!-- <p><i class="fa fa-chevron-right fa-1"></i><span><a href="EMBA-Placement-Brochure-2020" target="blank">Executive MBA Placement Brochure</a></span></p> -->
+		<!-- <p><i class="fa fa-chevron-right fa-1"></i><span><a href="pdf/VGSOM_MBA PROGRAMMEPartTimetwoYear Programme Brochure 29 Aug(Final)1.pdf" target="blank">Brochure</a></span></p>
+		<p><i class="fa fa-chevron-right fa-1"></i><span><a href="https://erp.iitkgp.ac.in/ERPWebServices/curricula/CurriculaSubjectsList.jsp?stuType=PG&splCode=BM1" target="blank">Curriculum</a></span></p> -->
+
+		<!-- <H4>PhD</H4>
+		<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"></i><span><a href="vgsom-phd.php">Overview</a></span></p>
+		<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"></i><span><a href="vgsom-phd.php#admission">Admission</a></span></p> -->
+
+		<!-- <H4>PGDBA</H4>
+		<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"></i><span><a href="pgdba.php">Overview</a></span></p>
+		<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"></i><span><a href="pgdba.php#dates">Important Dates</a></span></p>
+		<p><i class="fa fa-chevron-right fa-1" aria-hidden="true"></i><span><a href="http://www.pgdba.iitkgp.ac.in" target="blank">Website</a></span></p> -->
+
+		</div>
+
+	</div>
+</div>
+</div>
+
